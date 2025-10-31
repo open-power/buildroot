153ab664b7a282afb31098d5565422ee40a386f1
Nick Bofferding <opensource@bofferding.net>
2025-10-31 21:50:48 +0000

    This commit changes various packages' http source URLs to https to improve the security posture of dependent builds
    Signed-off-by: Nick Bofferding <opensource@bofferding.net>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LINUX_FIRMWARE_VERSION = 65b1c68c63f974d72610db38dfae49861117cae2
-LINUX_FIRMWARE_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+LINUX_FIRMWARE_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 LINUX_FIRMWARE_SITE_METHOD = git
 
 # Intel SST DSP
