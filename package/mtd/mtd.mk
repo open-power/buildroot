99d1237fb02b78c9496aa16a1899e3172b0c62b5
Nick Bofferding <opensource@bofferding.net>
2025-11-06 21:03:50 +0000

    This commit changes various packages' http source URLs to https to improve the security posture of dependent builds
    Updated packages include lvm2, libxml2, and mtd
    
    Signed-off-by: Nick Bofferding <opensource@bofferding.net>

--- package/mtd/mtd.mk
+++ package/mtd/mtd.mk
@@ -6,7 +6,7 @@
 
 MTD_VERSION = 2.0.1
 MTD_SOURCE = mtd-utils-$(MTD_VERSION).tar.bz2
-MTD_SITE = ftp://ftp.infradead.org/pub/mtd-utils
+MTD_SITE = https://infraroot.at/pub/mtd
 MTD_LICENSE = GPL-2.0
 MTD_LICENSE_FILES = COPYING
 MTD_INSTALL_STAGING = YES
